867d0e5b6f6c8e0f75e14fae00784ebef491008a
EniuK <108581363+EniuK@users.noreply.github.com>
2025-01-12 15:12:53 +0100

    add file with necessary imports

---
+++ weight-tracker-frontend/src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import "./confirmationModal.css";
+import { ConfirmationModalProps } from "../../types/CustomTypes.tsx"; // Import typów
+
