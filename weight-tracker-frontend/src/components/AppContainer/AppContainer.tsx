b96129730702f0a5ce76925ab936c095c1c70bfd
EniuK <108581363+EniuK@users.noreply.github.com>
2025-01-12 15:02:37 +0100

    add wrapper function with imports

---
+++ weight-tracker-frontend/src/components/AppContainer/AppContainer.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import CustomForm from "../Form/customForm.tsx";
+import WeightList from "../WeightList/WeightList.tsx";
+import { FormControlLabel, Switch } from "@mui/material";
+import axios from "axios";
+import "./appContainer.css";
+
